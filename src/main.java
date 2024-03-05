50a118f12c6ac84fa9c8e23dfdcfeda1077f64e5
BigBigMajesty <103768015+BigMajesty@users.noreply.github.com>
2024-03-05 22:58:25 +0800

    增加

---
+++ src/main.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: main
+ * Package: PACKAGE_NAME
+ * Description:
+ * Author: zhj
+ *
+ * @Create: 2024/3/5 - 22:55
+ * @Version: v1.0
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
